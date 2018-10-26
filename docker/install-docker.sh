634c656219f531862146f92803b8d004596d8729
moorara <moorara@gmail.com>
2018-10-25 21:29:14 -0400

    refactor docker scripts

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 # USAGE:
 #   ./install-docker.sh
-#   ./install-docker.sh -d 18.06.1-ce
-#   ./install-docker.sh --docker 18.06.1-ce
-#   ./install-docker.sh -d 18.06.1-ce -c 1.22.0
-#   ./install-docker.sh --docker 18.06.1-ce --compose 1.22.0
+#   ./install-docker.sh -r 18.06.1-ce
+#   ./install-docker.sh --release 18.06.1-ce
 #
 
 set -euo pipefail
@@ -33,52 +31,33 @@ function process_args {
   while [[ $# > 0 ]]; do
     key=$1
     case $key in
-      -d|--docker)
-      docker_version=$2
-      shift
-      ;;
-      -c|--compose)
-      compose_version=$2
+      -r|--release)
+      release=$2
       shift
       ;;
     esac
     shift
   done
 
-  docker_version=${docker_version:-$(curl -s https://download.docker.com/linux/static/stable/x86_64/ | grep -oe '[.0-9]*-ce' | tail -n 1)}
-  compose_version=${compose_version:-$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')}
+  release=${release:-$(curl -s https://download.docker.com/linux/static/stable/x86_64/ | grep -oe '[.0-9]*-ce' | tail -n 1)}
 }
 
 function install_docker {
-  printf "${blue}Installing docker ${docker_version} ...${nocolor}\n"
+  printf "${blue}Installing docker ${release} ...${nocolor}\n"
 
   os=$(uname -s | tr [:upper:] [:lower:])
   arch=$(uname -m)
-  file=./docker.tgz
+  archive=./docker.tgz
   path=/usr/local/bin/
 
-  curl -fsSL https://download.docker.com/${os}/static/stable/${arch}/docker-${docker_version}.tgz -o ${file}
-  tar -xz --strip-components=1 -f ${file} -C ${path}
-  rm ${file}
+  curl -fsSL https://download.docker.com/${os}/static/stable/${arch}/docker-${release}.tgz -o ${archive}
+  tar -xz --strip-components=1 -C ${path} -f ${archive}
+  rm ${archive}
 
-  printf " ${green}docker ${docker_version} installed successfully!${nocolor}\n"
-}
-
-function install_docker_compose {
-  printf "${blue}Installing docker-compose ${compose_version} ...${nocolor}\n"
-
-  os=$(uname -s)
-  arch=$(uname -m)
-  path=/usr/local/bin/docker-compose
-
-  curl -fsSL https://github.com/docker/compose/releases/download/${compose_version}/docker-compose-${os}-${arch} -o ${path}
-  chmod +x ${path}
-
-  printf " ${green}docker-compose ${compose_version} installed successfully!${nocolor}\n"
+  printf " ${green}docker ${release} installed successfully!${nocolor}\n"
 }
 
 
-ensure_command "grep" "tar" "curl" "jq"
+ensure_command "grep" "tar" "curl"
 process_args
 install_docker
-install_docker_compose
