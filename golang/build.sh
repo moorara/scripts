281caee75e210bbab7542e218f46ee92c72f4bd0
Milad Irannejad <milad.irannejad@flybits.com>
2018-11-10 22:17:00 -0500

    golang: update scripts

--- golang/build.sh
+++ golang/build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#
+# USAGE:
+#   ./build.sh -a
+#   ./build.sh -m main.go -b app
+#   ./build.sh --all --main main.go --binary builds/app
+#
+
 set -euo pipefail
 
 
@@ -12,16 +19,58 @@ version_flag="-X $(go list ./cmd/version).Version=$version"
 revision_flag="-X $(go list ./cmd/version).Revision=$revision"
 branch_flag="-X $(go list ./cmd/version).Branch=$branch"
 buildtime_flag="-X $(go list ./cmd/version).BuildTime=$buildtime"
+ldflags="$version_flag $revision_flag $branch_flag $buildtime_flag"
+
+platforms="linux-386 linux-amd64 darwin-386 darwin-amd64 windows-386 windows-amd64"
+
+
+function process_args {
+  while [[ $# > 0 ]]; do
+    key=$1
+    case $key in
+      -a|--all)
+      all=true
+      ;;
+      -m|--main)
+      main=$2
+      shift
+      ;;
+      -b|--binary)
+      binary=$2
+      shift
+      ;;
+    esac
+    shift
+  done
+
+  all=${all:-false}
+  main=${main:-"main.go"}
+  binary=${binary:-"build/app"}
+}
 
 function build_binary {
-  main_file=$1
-  binary_file=$2
-
   go build \
-    -o $binary_file \
-    -ldflags "$version_flag $revision_flag $branch_flag $buildtime_flag" \
-    $main_file
+    -ldflags "$ldflags" \
+    -o $binary \
+    $main
+}
+
+function cross_compile {
+  for platform in $platforms; do
+    GOOS=$(echo $platform | cut -d- -f1)
+	  GOARCH=$(echo $platform | cut -d- -f2)
+	  go build \
+      -ldflags "$ldflags" \
+      -o $binary-$platform \
+      $main
+  done
 }
 
 
-build_binary $1 $2
+process_args $@
+
+if [ "$all" == true ]; then
+  cross_compile
+else
+  build_binary
+fi
